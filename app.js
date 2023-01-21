762408ed8e7124aa676fa655a5c47e4583d958c5
ashupn10 <ashupn10@gmail.com>
2023-01-21 22:33:52 +0530

    Made User model

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize=require('./util/database');
 const errorController = require('./controllers/error');
+const product=require('./models/product');
+const user=require('./models/user');
 
 const app = express();
 
@@ -16,12 +18,32 @@ const { application } = require('express');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    user.findByPk(1)
+    .then(User=>{
+        req.user=User;
+        next();
+    })
+    .catch(err=>console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
+product.belongsTo(user,{constraints:true,onDelete:'CASCADE'});
+user.hasMany(product);
+
 sequelize.sync()
 .then(result=>{
-    app.listen(3000);
+    return user.findByPk(1);
+})
+.then(User=>{
+    if(!User){
+        return user.create({UserName:'test@g',email:'test',password:'password'});
+    }
+    return User;
+})
+.then(()=>{
+    app.listen(8001);
 })
 .catch(err=>console.log(err));
